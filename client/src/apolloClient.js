9673dfc09c5f0098af5f7752dc31892ae7e3f3fe
Hrayr <iamhrayr@gmail.com>
2018-07-09 02:56:34 +0400

    st

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -59,7 +59,7 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
     link: ApolloLink.from([
         stateLink,
-        authLink.concat(link),
+        authLink.concat(link)
     ]),
     cache
 });
